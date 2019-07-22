73d9cbdc463b75d4a75e1db049c8573d53cd0175
Lahm <goldguard822@gmail.com>
2019-07-22 17:28:23 +0900

    20190722_1728

--- showcase/js/global_init.js
+++ showcase/js/global_init.js
@@ -1,8 +1,8 @@
 function sc_global(){}
 sc_global.categories = "";
 sc_global.superCats = "";
-sc_global.catNames = "";
-sc_global.catToId = "";
+sc_global.catNames = [];
+sc_global.catToId = {};
 sc_global.idToCat = "";
 sc_global.iconSelected = [];
 sc_global.imIdList = [];
@@ -21,6 +21,7 @@ sc_global.init = function(){
 
 sc_global.setData = function(){
   //카테고리 정보 연동 해야함..
+  /*
   sc_global.categories = {
     'outdoor': [{'supercategory': 'outdoor', 'id': 10, 'name': 'traffic light'}, {'supercategory': 'outdoor', 'id': 11, 'name': 'fire hydrant'}, {'supercategory': 'outdoor', 'id': 13, 'name': 'stop sign'}, {'supercategory': 'outdoor', 'id': 14, 'name': 'parking meter'}, {'supercategory': 'outdoor', 'id': 15, 'name': 'bench'}], 'food': [{'supercategory': 'food', 'id': 52, 'name': 'banana'}, {'supercategory': 'food', 'id': 53, 'name': 'apple'}, {'supercategory': 'food', 'id': 54, 'name': 'sandwich'}, {'supercategory': 'food', 'id': 55, 'name': 'orange'}, {'supercategory': 'food', 'id': 56, 'name': 'broccoli'}, {'supercategory': 'food', 'id': 57, 'name': 'carrot'}, {'supercategory': 'food', 'id': 58, 'name': 'hot dog'}, {'supercategory': 'food', 'id': 59, 'name': 'pizza'}, {'supercategory': 'food', 'id': 60, 'name': 'donut'}, {'supercategory': 'food', 'id': 61, 'name': 'cake'}], 'indoor': [{'supercategory': 'indoor', 'id': 84, 'name': 'book'}, {'supercategory': 'indoor', 'id': 85, 'name': 'clock'}, {'supercategory': 'indoor', 'id': 86, 'name': 'vase'}, {'supercategory': 'indoor', 'id': 87, 'name': 'scissors'}, {'supercategory': 'indoor', 'id': 88, 'name': 'teddy bear'}, {'supercategory': 'indoor', 'id': 89, 'name': 'hair drier'}, {'supercategory': 'indoor', 'id': 90, 'name': 'toothbrush'}], 'appliance': [{'supercategory': 'appliance', 'id': 78, 'name': 'microwave'}, {'supercategory': 'appliance', 'id': 79, 'name': 'oven'}, {'supercategory': 'appliance', 'id': 80, 'name': 'toaster'}, {'supercategory': 'appliance', 'id': 81, 'name': 'sink'}, {'supercategory': 'appliance', 'id': 82, 'name': 'refrigerator'}], 'sports': [{'supercategory': 'sports', 'id': 34, 'name': 'frisbee'}, {'supercategory': 'sports', 'id': 35, 'name': 'skis'}, {'supercategory': 'sports', 'id': 36, 'name': 'snowboard'}, {'supercategory': 'sports', 'id': 37, 'name': 'sports ball'}, {'supercategory': 'sports', 'id': 38, 'name': 'kite'}, {'supercategory': 'sports', 'id': 39, 'name': 'baseball bat'}, {'supercategory': 'sports', 'id': 40, 'name': 'baseball glove'}, {'supercategory': 'sports', 'id': 41, 'name': 'skateboard'}, {'supercategory': 'sports', 'id': 42, 'name': 'surfboard'}, {'supercategory': 'sports', 'id': 43, 'name': 'tennis racket'}], 'animal': [{'supercategory': 'animal', 'id': 16, 'name': 'bird'}, {'supercategory': 'animal', 'id': 17, 'name': 'cat'}, {'supercategory': 'animal', 'id': 18, 'name': 'dog'}, {'supercategory': 'animal', 'id': 19, 'name': 'horse'}, {'supercategory': 'animal', 'id': 20, 'name': 'sheep'}, {'supercategory': 'animal', 'id': 21, 'name': 'cow'}, {'supercategory': 'animal', 'id': 22, 'name': 'elephant'}, {'supercategory': 'animal', 'id': 23, 'name': 'bear'}, {'supercategory': 'animal', 'id': 24, 'name': 'zebra'}, {'supercategory': 'animal', 'id': 25, 'name': 'giraffe'}], 'vehicle': [{'supercategory': 'vehicle', 'id': 2, 'name': 'bicycle'}, {'supercategory': 'vehicle', 'id': 3, 'name': 'car'}, {'supercategory': 'vehicle', 'id': 4, 'name': 'motorcycle'}, {'supercategory': 'vehicle', 'id': 5, 'name': 'airplane'}, {'supercategory': 'vehicle', 'id': 6, 'name': 'bus'}, {'supercategory': 'vehicle', 'id': 7, 'name': 'train'}, {'supercategory': 'vehicle', 'id': 8, 'name': 'truck'}, {'supercategory': 'vehicle', 'id': 9, 'name': 'boat'}], 'furniture': [{'supercategory': 'furniture', 'id': 62, 'name': 'chair'}, {'supercategory': 'furniture', 'id': 63, 'name': 'couch'}, {'supercategory': 'furniture', 'id': 64, 'name': 'potted plant'}, {'supercategory': 'furniture', 'id': 65, 'name': 'bed'}, {'supercategory': 'furniture', 'id': 67, 'name': 'dining table'}, {'supercategory': 'furniture', 'id': 70, 'name': 'toilet'}], 'accessory': [{'supercategory': 'person', 'id': 1, 'name': 'person'}, {'supercategory': 'accessory', 'id': 27, 'name': 'backpack'}, {'supercategory': 'accessory', 'id': 28, 'name': 'umbrella'}, {'supercategory': 'accessory', 'id': 31, 'name': 'handbag'}, {'supercategory': 'accessory', 'id': 32, 'name': 'tie'}, {'supercategory': 'accessory', 'id': 33, 'name': 'suitcase'}], 'electronic': [{'supercategory': 'electronic', 'id': 72, 'name': 'tv'}, {'supercategory': 'electronic', 'id': 73, 'name': 'laptop'}, {'supercategory': 'electronic', 'id': 74, 'name': 'mouse'}, {'supercategory': 'electronic', 'id': 75, 'name': 'remote'}, {'supercategory': 'electronic', 'id': 76, 'name': 'keyboard'}, {'supercategory': 'electronic', 'id': 77, 'name': 'cell phone'}], 'kitchen': [{'supercategory': 'kitchen', 'id': 44, 'name': 'bottle'}, {'supercategory': 'kitchen', 'id': 46, 'name': 'wine glass'}, {'supercategory': 'kitchen', 'id': 47, 'name': 'cup'}, {'supercategory': 'kitchen', 'id': 48, 'name': 'fork'}, {'supercategory': 'kitchen', 'id': 49, 'name': 'knife'}, {'supercategory': 'kitchen', 'id': 50, 'name': 'spoon'}, {'supercategory': 'kitchen', 'id': 51, 'name': 'bowl'}]
   };
@@ -40,23 +41,38 @@ sc_global.setData = function(){
   sc_global.idToCat = {
     1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag', 32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard', 37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove', 41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle', 46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'
   };
+  */
+
+  sc_global.categories = [
+    {'id': 1, 'name': '탄약'}, {'id': 2, 'name': '폭죽'}, {'id': 3, 'name': '라이터'}, {'id': 4, 'name': '에어로졸'}, {'id': 5, 'name': '휴대용가스'}, {'id': 6, 'name': '알코올'}, {'id': 7, 'name': '신나'}, {'id': 8, 'name': '라이터기름'}, {'id': 9, 'name': '성냥'}
+    , {'id': 10, 'name': '고체연료'}, {'id': 11, 'name': '건전지'}, {'id': 12, 'name': '휴대용 배터리'}, {'id': 13, 'name': '손톱깎이'}, {'id': 14, 'name': '칼'}, {'id': 15, 'name': '톱'}, {'id': 16, 'name': '가위'}, {'id': 17, 'name': '송곳'}, {'id': 18, 'name': '드라이버'}, {'id': 19, 'name': '스패너'}
+    , {'id': 20, 'name': '액체(100ml이상)'}, {'id': 21, 'name': '수갑'}, {'id': 22, 'name': '도끼'}, {'id': 23, 'name': '망치'}, {'id': 24, 'name': '야구배트'}, {'id': 25, 'name': '총기'}, {'id': 26, 'name': '금속 파이프'}, {'id': 27, 'name': '총기부품'}, {'id': 28, 'name': '핸드폰'}, {'id': 29, 'name': 'USB'}
+    , {'id': 30, 'name': '노트북'}, {'id': 31, 'name': 'Tablet'}, {'id': 32, 'name': 'SSD'}, {'id': 33, 'name': 'HDD'}, {'id': 34, 'name': '외장하드'}
+  ];
+  $.each(sc_global.categories, function(i,v){
+    //console.log(sc_global.categories[i]);
+    sc_global.catNames.push($(this)[0].name);
+    sc_global.catToId[$(this)[0].name] = $(this)[0].id;
+    sc_global.idToCat[$(this)[0].id] = $(this)[0].name;
+  });
 }
 
 //icon drawing 및 event binding
 sc_global.drawIcon = function() {
   // icong HTML drawing
   var div = '', i, j;
-  $.each(sc_global.superCats, function(i,v){
+  //$.each(sc_global.superCats, function(i,v){
     div += '<div class="iconSubpanel">';
-    var cats = sc_global.categories[sc_global.superCats[i]];
+    //var cats = sc_global.categories[sc_global.superCats[i]];
+    var cats = sc_global.categories;
     $.each(cats,function(j,v){
   //    div += '<span class="iconSelectWrap">';
       div += '<img title="' + cats[j].name + '" id="icon_' + cats[j].id;
-      div += '" class="iconSelect" src="./images/icons/'+cats[j].id+'.jpg" onerror="sc_global.imgError()"/>';
+      div += '" class="iconSelect" src="./images/icons/new/'+cats[j].id+'.jpg" onerror="sc_global.imgError()"/>';
   //    div += '</span>'
     });
     div += '</div>';
-  });
+  //});
   $('#sc_IconPanel').append(div);
 
   // icon event binding
@@ -64,7 +80,8 @@ sc_global.drawIcon = function() {
   $.each(icons, function(i,v){
     var icon = $(this);
     icon.data('state', false);
-    icon.off('mousedown').on('mousedown', function(){
+    icon.off('mousedown').on('mousedown', function(e){
+      if(e.button != 0) {return;}
       var state = $(this).data('state');
       var cat = $(this).attr('title');
       if(state) {
@@ -91,7 +108,7 @@ sc_global.drawIcon = function() {
       var icon = $(this);
       if (icon.attr('title') == tag) {
         icon.data('state', state);
-        var color = state?'#2177aa':'transparent';
+        var color = state?'#2177aa':'#ccc';
         icon.css('border-color', color);
       }
     });
