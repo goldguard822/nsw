87872c1228183c5dadc0857817e17b29e7768788
Lahm <goldguard822@gmail.com>
2019-08-01 17:29:02 +0900

    20190801_1728

--- homepage/js/mpole_global.js
+++ homepage/js/mpole_global.js
@@ -1,10 +1,40 @@
 function mpole_global(){};
+mpole_global.swiper = "";
 mpole_global.init = function() {
+  mpole_global.setSwiper();
   mpole_global.setResize();
   mpole_global.setScroll();
   mpole_global.locationBind();
   mpole_global.setGnb();
   mpole_global.eventBind();
+}
+
+mpole_global.setSwiper = function() {
+  swiper = new Swiper({
+    el: '.swiper-container',
+    initialSlide: 2,
+    spaceBetween: 50,
+    slidesPerView: 2,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    grabCursor: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    mousewheel: {
+      enabled: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
 }
 
 mpole_global.setResize = function() {
